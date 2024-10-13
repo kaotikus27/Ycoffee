84170f7337510aa4aee0e6ff735f211510b056db
ELEOMAR <kaotikus27@gmail.com>
2024-10-13 14:32:40 +0800

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingpageComponent } from './components/pages/landing/landingpage.component';
+import { MenuComponent } from './components/pages/menu/menu.component';
+import { NavbarComponent } from './components/pages/navbar/navbar.component';
 
 const routes: Routes = [
   {
     path:'',
     component:LandingpageComponent
-  }
+  },
+  {
+    path:'menu',
+    component:MenuComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/pages/footer/footer.component';
 import { LandingpageComponent } from './components/pages/landing/landingpage.component';
 import { NavbarComponent } from './components/pages/navbar/navbar.component';
+import { MenuComponent } from './components/pages/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingpageComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
